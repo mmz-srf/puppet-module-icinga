5c265868a7425232b13369104852cb1c273ed34d
Simon Josi <simon.josi@atizo.com>
2011-02-14 14:47:03 +0100

    fix typo

--- manifests/defaults/timeperiods.pp
+++ manifests/defaults/timeperiods.pp
@@ -8,7 +8,7 @@ class icinga::defaults::timeperiods {
       thursday    => '00:00-24:00',
       friday      => '00:00-24:00',
       saturday    => '00:00-24:00';
-      sunday      => '00:00-24:00',
+      sunday      => '00:00-24:00';
     'workhours':
       alias       => 'Standard Work Hours',
       monday      => '09:00-19:00',
@@ -24,7 +24,7 @@ class icinga::defaults::timeperiods {
       thursday    => '00:00-09:00,19:00-24:00',
       friday      => '00:00-09:00,19:00-24:00',
       saturday    => '00:00-24:00',
-      sunday      => '00:00-24:00',
+      sunday      => '00:00-24:00';
     'never':
       alias       => 'Never';
   }
