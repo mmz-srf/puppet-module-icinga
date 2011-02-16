57a0735971be5c8c31196508fd87103576d14a0f
Simon Josi <simon.josi@atizo.com>
2011-02-16 19:42:17 +0100

    update icinca module

--- manifests/pnp4nagios.pp
+++ manifests/pnp4nagios.pp
@@ -2,9 +2,22 @@ class icinga::pnp4nagios {
   package{'pnp4nagios':
     ensure => present,
   }
-  file{'/var/icinga/spool':
-    ensure => directory,
-    require => Package['icinga'],
-    owner => icinga, group => icinga, mode => 0755;
+  file{
+    '/etc/pnp4nagios/npcd.cfg':
+      source => [
+        "puppet://$server/modules/site-icinga/pnp4nagios/$fqdn/npcd.cfg",
+        "puppet://$server/modules/site-icinga/pnp4nagios/npcd.cfg",
+        "puppet://$server/modules/icinga/pnp4nagios/npcd.cfg",
+      ],
+      require => Package['icinga'],
+      owner => icinga, group => icinga, mode => 0755;
+  }
+  file{[
+    '/var/icinga/spool',
+    '/var/lib/pnp4nagios',
+  ]:
+      ensure => directory,
+      require => Package['icinga'],
+      owner => icinga, group => icinga, mode => 0755;
   }
 }
