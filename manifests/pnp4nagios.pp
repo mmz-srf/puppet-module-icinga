b599b7ff31c175d4232e87db438256b75171c7f7
Simon Josi <simon.josi@atizo.com>
2011-02-16 20:37:52 +0100

    add pnp4nagios

--- manifests/pnp4nagios.pp
+++ manifests/pnp4nagios.pp
@@ -10,7 +10,19 @@ class icinga::pnp4nagios {
         "puppet://$server/modules/icinga/pnp4nagios/npcd.cfg",
       ],
       require => Package['icinga'],
-      owner => icinga, group => icinga, mode => 0755;
+      owner => root, group => root, mode => 0444;
+    '/usr/share/icinga-web/app/modules/Cronks/data/xml/grid/icinga-host-template.xml':
+      source => "puppet://$server/modules/icinga/pnp4nagios/icinga-host-template.xml",
+      require => Package['icinga-web'],
+      owner => root, group => root, mode => 0644;
+    '/usr/share/icinga-web/app/modules/Cronks/data/xml/grid/icinga-service-template.xml':
+      source => "puppet://$server/modules/icinga/pnp4nagios/icinga-service-template.xml",
+      require => Package['icinga-web'],
+      owner => root, group => root, mode => 0644;
+    '/usr/share/icinga-web/app/modules/Cronks/config/cronks.xml':
+      source => "puppet://$server/modules/icinga/pnp4nagios/cronks.xml",
+      require => Package['icinga-web'],
+      owner => root, group => root, mode => 0644;
   }
   file{[
     '/var/icinga/spool',
@@ -19,5 +31,17 @@ class icinga::pnp4nagios {
       ensure => directory,
       require => Package['icinga'],
       owner => icinga, group => icinga, mode => 0755;
+  }
+  exec{'icinga-web_clearcache':
+    command => '/usr/share/icinga-web/bin/clearcache.sh',
+    refershonly => true,
+    require => Package['icinga-web'],
+    subscribe => [
+      File[
+        '/usr/share/icinga-web/app/modules/Cronks/data/xml/grid/icinga-host-template.xml',
+        '/usr/share/icinga-web/app/modules/Cronks/data/xml/grid/icinga-service-template.xml',
+        '/usr/share/icinga-web/app/modules/Cronks/config/cronks.xml',
+      ]
+    ]
   }
 }
