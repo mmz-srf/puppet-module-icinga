658bbfbc7e709c7fac7be64882c17d31648e6123
Simon Josi <simon.josi@atizo.com>
2011-02-17 14:25:26 +0100

    tune default commands

--- manifests/defaults/commands.pp
+++ manifests/defaults/commands.pp
@@ -1,76 +1,64 @@
 class icinga::defaults::commands {
   # common service commands
   nagios_command{
-    check_dummy:
+    'check_dummy':
       command_line => '$USER1$/check_dummy $ARG1$';
-    check_ping:
+    'check_ping':
       command_line => '$USER1$/check_ping -H $HOSTADDRESS$ -w $ARG1$ -c $ARG2$';
-    check-host-alive:
+    'check-host-alive':
       command_line => '$USER1$/check_ping -H $HOSTADDRESS$ -w 3000,80% -c 5000,100% -p 1';
-    check_tcp:
+    'check_tcp':
       command_line => '$USER1$/check_tcp -H $HOSTADDRESS$ -p $ARG1$';
-    check_udp:
+    'check_udp':
       command_line => '$USER1$/check_udp -H $HOSTADDRESS$ -p $ARG1$';
-    check_load:
+    'check_load':
       command_line => '$USER1$/check_load -w $ARG1$ -c $ARG2';
-    check_dig:
+    'check_dig':
      command_line => '$USER1$/check_dig -H $HOSTADDRESS$ -l $ARG1$';
-    check_disk:
+    'check_disk':
       command_line => '$USER1$/check_disk -w $ARG1$ -c $ARG2$ -e -p $ARG3$';
-    check_all_disks:
+    'check_all_disks':
       command_line => '$USER1$/check_disk -w $ARG1$ -c $ARG2$ -e';
-    check_ssh:
+    'check_ssh':
       command_line => '$USER1$/check_ssh $HOSTADDRESS$';
-    check_ssh_port:
+    'check_ssh_port':
       command_line => '$USER1$/check_ssh -p $ARG1$ $HOSTADDRESS$';
-    check_http:
-      command_line => '$USER1$/check_http -H $HOSTADDRESS$ -I $HOSTADDRESS$';
-    check_https:
-      command_line => '$USER1$/check_http --ssl -H $HOSTADDRESS$ -I $HOSTADDRESS$';
-    check_https_cert:
-      command_line => '$USER1$/check_http --ssl -C 20 -H $HOSTADDRESS$ -I $HOSTADDRESS$';
-    check_http_url:
+    'check_http':
+      command_line => '$USER1$/check_http -H $HOSTADDRESS$';
+    'check_http_url':
       command_line => '$USER1$/check_http -H $ARG1$ -u $ARG2$';
-    check_http_url_regex:
+    'check_http_url_regex':
       command_line => '$USER1$/check_http -H $ARG1$ -u $ARG2$ -e $ARG3$';
-    check_http_url_string:
+    'check_http_url_string':
       command_line => '$USER1$/check_http -H $ARG1$ -u $ARG2$ -s $ARG3$';
-    check_https_url:
+    'check_https':
+      command_line => '$USER1$/check_http --ssl -H $HOSTADDRESS$';
+    'check_https_cert':
+      command_line => '$USER1$/check_http --ssl -C 20 -H $HOSTADDRESS$ -I $HOSTADDRESS$';
+    'check_https_url':
       command_line => '$USER1$/check_http --ssl -H $ARG1$ -u $ARG2$';
-    check_https_url_regex:
+    'check_https_url_regex':
       command_line => '$USER1$/check_http --ssl -H $ARG1$ -u $ARG2$ -e $ARG3$';
-    check_mysql:
+    'check_mysql':
       command_line => '$USER1$/check_mysql -H $ARG1$ -P $ARG2$ -u $ARG3$ -p $ARG4$';
-    check_mysql_db:
+    'check_mysql_db':
       command_line => '$USER1$/check_mysql -H $ARG1$ -P $ARG2$ -u $ARG3$ -p $ARG4$ -d $ARG5$';
-    check_ntp_time:
+    'check_ntp_time':
       command_line => '$USER1$/check_ntp_time -H $HOSTADDRESS$ -w 0.5 -c 1';
-    check_nrpe:
+    'check_nrpe':
       command_line => '$USER1$/check_nrpe -t 60 -H $HOSTADDRESS$ -c $ARG1$ -a $ARG2$';
-    check_nrpe_1arg:
-      command_line => '$USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$';
-    check_silc:
+    'check_silc':
       command_line => '$USER1$/check_tcp -p 706 -H $ARG1$';
-    check_smtp:
+    'check_smtp':
       command_line => '$USER1$/check_smtp -H $ARG1$';
-    check_sobby:
+    'check_sobby':
       command_line => '$USER1$/check_tcp -H $ARG1$ -p $ARG2$';
-    check_traceroute:
+    'check_traceroute':
       command_line => 'sudo $USER1$/check_traceroute.pl -H $ARG1$ -R $ARG2$';
-    check_traceroute_max_hops:
+    'check_traceroute_max_hops':
       command_line => 'sudo $USER1$/check_traceroute.pl -H $ARG1$ -R $ARG2$ -N $ARG3$';
-    check_jabber:
+    'check_jabber':
       command_line => '$USER1$/check_jabber -H $ARG1$';
-  }
-
-  # commands for services defined by other modules
-  nagios_command{
-    # from apache module
-    http_port:
-      command_line => '$USER1$/check_http -p $ARG1$ -H $HOSTADDRESS$ -I $HOSTADDRESS$';
-    # from bind module
-    check_dig2:
-      command_line => '$USER1$/check_dig -H $HOSTADDRESS$ -l $ARG1$ --record_type=$ARG2$';
   }
 
   # notification commands
