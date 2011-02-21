76ab97c4680e94e569d16f3599d2939094fe7652
Simon Josi <simon.josi@atizo.com>
2011-02-21 11:13:13 +0100

    try to eradicate cyclic dependencies

--- manifests/pnp4nagios.pp
+++ manifests/pnp4nagios.pp
@@ -25,7 +25,6 @@ class icinga::pnp4nagios {
         "puppet://$server/modules/site-icinga/pnp4nagios/apache.conf",
         "puppet://$server/modules/icinga/pnp4nagios/apache.conf",
       ],
-      notify => Package['httpd'],
       owner => root, group => root, mode => 0444;
     '/usr/share/icinga-web/app/modules/Cronks/data/xml/grid/icinga-host-template.xml':
       source => "puppet://$server/modules/icinga/pnp4nagios/icinga-host-template.xml",
