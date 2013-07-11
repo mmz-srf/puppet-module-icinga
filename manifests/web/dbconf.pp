2bdfd1941f915ccc0c45a6ad9d46267f88418a91
Andreas Zuber <zuber@puzzle.ch>
2013-07-11 18:05:05 +0200

    add class to manage icinga-web database config

---
+++ manifests/web/dbconf.pp
@@ -0,0 +1,19 @@
+# deploy db config for icinga-web
+class icinga::web::dbconf (
+  $database = 'icinga_web',
+  $username = 'icinga_web',
+  $password = 'icinga_web',
+  $hostname = 'localhost',
+  $port     = '3306'
+){
+
+  file{'/etc/icinga-web/databases.xml':
+    content => template('icinga/icinga-web/databases.xml.erb'),
+    owner   => root,
+    group   => apache,
+    mode    => '0640',
+    require => Package['icinga-web'],
+    notify  => Service['apache']
+  }
+
+}
