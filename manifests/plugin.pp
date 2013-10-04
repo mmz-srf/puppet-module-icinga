0f1547db467fc4ab4c4be5bf2513357adb93876b
Simon Josi <me@yokto.net>
2013-10-04 11:42:56 +0200

    plugin deployment: introduce source_module parameter

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,14 +1,13 @@
-define icinga::plugin {
+define icinga::plugin(
+  source_module => 'icinga',
+) {
   $libdir = $architecture ? {
     x86_64 => 'lib64',
     default => 'lib',
   }
   include ::icinga::plugins
   file{"/usr/$libdir/nagios/plugins/$name":
-    source => [
-      "puppet://$server/modules/site_icinga/plugins/$name",
-      "puppet://$server/modules/icinga/plugins/$name",
-    ],
+    source => "puppet://$server/modules/${source_module}/plugins/${name}",
     require => Package['nagios-plugins'],
     owner => root, group => root, mode => 0755;
   }
