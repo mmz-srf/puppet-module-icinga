b1cbaa90cdb2dd83a64f51cb7bd3eecdba9508d4
mms-gianni <gianni.carafa@srf.ch>
2019-01-15 14:42:51 +0100

    this Package has a new name since Debian 8

--- manifests/plugins/debian.pp
+++ manifests/plugins/debian.pp
@@ -1,7 +1,19 @@
 class icinga::plugins::debian {
-  package{'nagios-plugins':
-    name => 'nagios-plugins',
-    ensure => present,
-    install_options => ['-t','wheezy'],
+
+  
+  if $::lsbdistcodename == 'buster' {
+    # This package was renamed in Debian8 Jessie. We want to use the new shit
+    # TODO : Change this if to Busters Lsb major dist number aufter buster release
+    package{'nagios-plugins':
+      name => 'monitoring-plugins',
+      ensure => present,
+    }
+  } else {
+
+    package{'nagios-plugins':
+      name => 'nagios-plugins',
+      ensure => present,
+      install_options => ['-t','wheezy'],
+    }
   }
 }
