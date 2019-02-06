e01cea2754a719917d5fb555c151be8619dfd558
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2019-02-06 08:14:02 +0100

    remove no longer needed wheezy backuport

--- manifests/plugins/debian.pp
+++ manifests/plugins/debian.pp
@@ -1,19 +1,17 @@
 class icinga::plugins::debian {
-
   
   if $::lsbdistcodename == 'buster' {
     # This package was renamed in Debian8 Jessie. We want to use the new shit
     # TODO : Change this if to Busters Lsb major dist number aufter buster release
     package{'nagios-plugins':
-      name => 'monitoring-plugins',
+      name   => 'monitoring-plugins',
       ensure => present,
     }
   } else {
 
     package{'nagios-plugins':
-      name => 'nagios-plugins',
+      name   => 'nagios-plugins',
       ensure => present,
-      install_options => ['-t','wheezy'],
     }
   }
 }
