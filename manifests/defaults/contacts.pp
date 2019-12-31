526543c18eaf42f979749cb587c6eecfce47e56a
srf-stefan <stefan.krebs@srf.ch>
2019-12-31 12:15:08 +0100

    made notification options configurable
    PLATFROM-206

--- manifests/defaults/contacts.pp
+++ manifests/defaults/contacts.pp
@@ -1,19 +1,13 @@
-class icinga::defaults::contacts {
-  $notify_or_not_service = $::mpc_project ? {
-    'production' => 'w,u,c,r',
-    default      => 'n',
-  }
-
-  $notify_or_not_host = $::mpc_project ? {
-    'production' => 'd,r',
-    default      => 'n',
-  }
+class icinga::defaults::contacts (
+  $notify_option_service       = 'w,u,c,r',
+  $notify_option_host          = 'd,r',
+  ) {
   nagios_contact{'root':
     alias                           => 'Root',
     service_notification_period     => '24x7',
     host_notification_period        => '24x7',
-    service_notification_options    => $notify_or_not_service,
-    host_notification_options       => $notify_or_not_host,
+    service_notification_options    => $notify_option_service,
+    host_notification_options       => $notify_option_host,
     service_notification_commands   => 'notify-service-by-email',
     host_notification_commands      => 'notify-host-by-email',
     email                           => 'root@localhost',
