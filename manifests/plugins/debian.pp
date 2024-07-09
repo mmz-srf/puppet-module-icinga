c56111080ff507c0b087fb782d89c38d37fa7afd
Gianni Carafa <gianni.carafa@srf.ch>
2024-07-09 11:10:50 +0200

    remove old debian 8 stuff

--- manifests/plugins/debian.pp
+++ manifests/plugins/debian.pp
@@ -1,17 +1,6 @@
 class icinga::plugins::debian {
-  
-  if $::lsbdistcodename == 'buster' {
-    # This package was renamed in Debian8 Jessie. We want to use the new shit
-    # TODO : Change this if to Busters Lsb major dist number aufter buster release
-    package{'nagios-plugins':
-      name   => 'monitoring-plugins',
-      ensure => present,
-    }
-  } else {
-
-    package{'nagios-plugins':
-      name   => 'nagios-plugins',
-      ensure => present,
-    }
+  package{'nagios-plugins':
+    name   => 'monitoring-plugins',
+    ensure => present,
   }
 }
